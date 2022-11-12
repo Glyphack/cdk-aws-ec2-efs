fdc77b1b8799b17f780e9de7a991804a212476b2
glyphack <sh.hooshyari@gmail.com>
2022-11-12 21:41:39 +0330

    Add aws-cdk keyword for construct.dev discovery

--- .projenrc.js
+++ .projenrc.js
@@ -1,14 +1,15 @@
-const { awscdk } = require('projen');
+const { awscdk } = require("projen");
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'glyphack',
-  authorAddress: 'sh.hooshyari@gmail.com',
-  cdkVersion: '2.50.0',
-  defaultReleaseBranch: 'main',
-  name: 'cdk-aws-ec2-efs',
-  repositoryUrl: 'https://github.com/sh.hooshyari/cdk-aws-ec2-efs.git',
+  author: "glyphack",
+  authorAddress: "sh.hooshyari@gmail.com",
+  cdkVersion: "2.50.0",
+  defaultReleaseBranch: "main",
+  name: "cdk-aws-ec2-efs",
+  repositoryUrl: "https://github.com/sh.hooshyari/cdk-aws-ec2-efs.git",
   python: {
-    distName: 'cdk-aws-ec2-efs',
-    module: 'cdk_aws_ec2_efs',
+    distName: "cdk-aws-ec2-efs",
+    module: "cdk_aws_ec2_efs",
   },
+  keywords: ["cdk", "aws-cdk"],
 });
 project.synth();
